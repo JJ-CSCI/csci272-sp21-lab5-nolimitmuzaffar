44fa7c21547dc4021dbf94fc937bc87c8e52eb65
Muzaffar Mukhitdinov pd 7 <mmukhitdinov7344@bths.edu>
2021-03-17 22:58:23 +0000

    lab5

--- main.cpp
+++ main.cpp
@@ -5,6 +5,65 @@
 //------------------------------
 
 // Write the assignment code here
+class Real{
+
+protected: //it can't be a private, because private var can't be accessed from inheritance
+float real_value = 0; //the numbers passed are float numbers
+  public:
+    float GetReal(){//to get the value
+    return real_value;
+  }
+  Real(float one){//constructor to store 1 real coefficient 
+    real_value = one;
+  }
+
+     Real operator*(const float&xyz){
+     real_value=real_value*xyz;
+     return *this;//returning a reference to the object
+  }
+};
+//This existing class Real is the base class, and the new class is going to be the derived class
+
+class Complex: public Real{
+  protected: //because private varriables can't be accessed from inheritance 
+  float complex_value = 0;  //the numbers passed are float numbers
+  public:
+  float GetImaginary(){//accesor  method
+     return complex_value;
+  }
+    Complex(float one, float two):Real(one){// the constructor. complex class is being derived
+    complex_value = two;
+      }
+  Complex operator*(const float&ABC){
+    real_value= real_value*ABC;
+    complex_value=complex_value* ABC;
+  //this means pointer to the object
+    return *this;
+  }
+};
+//this is derived class 
+class Surreal: public Complex{
+  float surreal_value= 0;   //the numbers passed are float numbers
+   public:
+   float GetSurreal(){//accesor function to return third value
+     return surreal_value;
+  }
+//constructor
+   Surreal(float one, float two, float three):Complex(one, two){
+     surreal_value = three;
+    }
+  //note : in c++ a class can inherit from more than one class 
+
+  Surreal operator*(const float&ABC){
+    real_value= real_value*ABC;
+    complex_value=complex_value*ABC;
+    surreal_value=surreal_value*ABC;
+//return a reference to the object
+    return *this; 
+  }
+
+
+};
 
 
 //------------------------------
